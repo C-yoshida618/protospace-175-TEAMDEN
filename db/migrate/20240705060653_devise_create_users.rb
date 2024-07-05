c747906cd60156e3e79293ebb984fce9425ee2e2
C-yoshida618 <kanamakihira@gmail.com>
2024-07-05 16:54:46 +0900

    ビューファイル作成

--- db/migrate/20240705060653_devise_create_users.rb
+++ db/migrate/20240705060653_devise_create_users.rb
@@ -6,10 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name,  null: false, default: ""
-      t.text :profile,            
-      t.text :occupation,          
-      t.text :position,       
+      t.string :name,               null: false, default: ""
+      t.text :profile
+      t.text :occupation
+      t.text :position
 
       ## Recoverable
       t.text   :reset_password_token
