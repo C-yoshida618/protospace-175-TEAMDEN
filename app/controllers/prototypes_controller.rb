d3029066bb1d4a4cf9265efdebe74286e70a9b99
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-06 12:39:03 +0900

    アクションとルーティングを設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,4 +2,10 @@ class PrototypesController < ApplicationController
   def index
     @prototypes = Prototype.all
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :prototypes, only: :index
+  resources :prototypes, only: [:index, :new, :create]
 end
