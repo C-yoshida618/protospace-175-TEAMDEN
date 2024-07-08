1077e8eb342abf7d2b5eec856593ef741dad00e4
C-yoshida618 <kanamakihira@gmail.com>
2024-07-08 19:36:37 +0900

    update Action/render

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -36,8 +36,14 @@ def edit
   end
 end
 
-  def update
+def update
+  @prototype = Prototype.find(params[:id])
+  if @prototype.update(prototype_params)
+    redirect_to prototype_path
+  else
+    render :edit
   end
+end
 
   private
 
