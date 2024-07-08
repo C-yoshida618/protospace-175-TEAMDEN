4784eaf5e26280ad4b47656cf0d1563a1295ec29
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-08 21:03:20 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ has_many :prototypes
         
-         validates :name,:password,:email, presence: true
+  validates :name,:password,:email, presence: true
 end
 
