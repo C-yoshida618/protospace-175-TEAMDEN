27ad171debdf5b9498a614d40c09f68fe3c9c6ed
yutaka41249 <yutaka.oda0821@gmail.com>
2024-07-09 21:11:55 +0900

    画像一覧表示追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"prototypes#index"
-  resources :prototypes , only:[:index,:new,:create,:destroy]
+  
 
-  resources :prototype do
+  resources :prototypes do
   resources :comments, only: :create
 
   end
