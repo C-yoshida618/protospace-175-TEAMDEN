cbaa174914ca87c9121da403dbe60f6d979e62fe
C-yoshida618 <kanamakihira@gmail.com>
2024-07-09 16:26:10 +0900

    ユーザー詳細ページに遷移
    できてる？？

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
 
   has_many :comments, dependent: :destroy
+  has_many :prototypes
   validates :text, presence: true
 end
