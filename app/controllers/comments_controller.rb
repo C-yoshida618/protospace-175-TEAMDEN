63673623b5e6b1f7a73030fce198fa29d0666491
C-yoshida618 <kanamakihira@gmail.com>
2024-07-09 14:54:28 +0900

    user_idとprototype_idもmerge

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,13 +4,12 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      @tweet = @comment.prototype
-      @comments = @prototype.comments
+      @comments = @prototype.comments.includes(:user)
       render "prototype/show" 
     end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:prototype_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
