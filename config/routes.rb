e8df3230252620c5ef78958efa72658c1be67785
C-yoshida618 <kanamakihira@gmail.com>
2024-07-10 13:03:01 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"prototypes#index"
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
 
-  resources :prototype do
   resources :comments, only: :create
   end
   resources :users , only: :show
