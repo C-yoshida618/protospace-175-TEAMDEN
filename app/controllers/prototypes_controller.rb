062f040079e3e306f64fb4fa97f5ce31eebfa46a
C-yoshida618 <kanamakihira@gmail.com>
2024-07-10 15:13:20 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -45,7 +45,7 @@ class PrototypesController < ApplicationController
   def destroy
     prototype = Prototype.find(params[:id])
     prototype.destroy
-    redirect_to root_path
+    redirect_to action: :index
   end
 
   private
